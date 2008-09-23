a4fa1d08ec878f7a6fdb12cca401c3e72637a67e
adamchuk <adamchuk@6ce74b2a-8879-11dd-a143-0dd59a09f4aa>
2008-09-23 09:30:03 +0000

    Initial plugin

---
+++ trunk/www/wp-content/plugins/cyfral/cyfral.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Plugin Name: Solvek Cyfral
+Plugin URI: http://code.google.com/p/cyfral/
+Description: Online collaborative tool
+Version: 0.1
+Author: Sergi Adamchuk
+Author URI: http://www.solvek.com
+*/
+
+$cyfral_db_version = "0.1";
+
+function cyfral_install () {
+   global $wpdb;
+   global $jal_db_version;
+
+   $table_name = $wpdb->prefix . "projects";
+   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+      
+      $sql = "CREATE TABLE " . $table_name . " (
+	  ID bigint(20) NOT NULL,
+	  volume int(11) NOT NULL,
+	  PRIMARY KEY  (ID)
+	);";
+
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      dbDelta($sql);
+
+      add_option("cyfral_db_version", $cyfral_db_version);
+
+   }
+}
+
+register_activation_hook(__FILE__,'cyfral_install');
+
+function cyfral_project_editor($post_id)
+{
+	echo '<div class="postbox">' . "\n";	
+	echo "<h3><a class='togbox'>+</a>".__("Work Volume", "cyfral")."</h3>\n";
+	echo '<div class="inside">' . "\n";
+	echo '<label for="volumework">'.__("Work Volume", "cyfral").': </label><input type="text" name="volumework" style="width: 40px" id="volumework" value="" /><span>'.__(" quantums", "cyfral").'</span>'. "\n";	
+	echo '<p>'.__("Quantums of work. Enter a number from 1 to 100000.", "cyfral").'</p>';
+	echo "</div>\n";
+	echo "</div>\n";
+}
+
+add_action('edit_form_advanced', 'cyfral_project_editor');
+
+function cyfral_save_project_info($post_id, $post)
+{
+	global $wpdb;
+	
+	$val = intval(stripslashes($_POST["volumework"]));
+	
+	//print("REPLACE {$wpdb->prefix}projects (ID, Volume) VALUES ($post_id, $val)");
+	$wpdb->query("REPLACE {$wpdb->prefix}projects (ID, Volume) VALUES ($post_id, $val)");
+	
+	return $post;
+}
+
+add_action('save_post', 'cyfral_save_project_info', 11, 2);
+
+?>
